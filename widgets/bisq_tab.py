a6210062c404d390a859693dfdc42f6afe460e1e
thecockatiel <thecockatiel@proton.me>
2024-10-15 13:38:48 +0330

    chore: add sell tab

--- widgets/bisq_tab.py
+++ widgets/bisq_tab.py
@@ -7,6 +7,7 @@ from electrum.i18n import _
 from electrum.logging import Logger
 from .tabs.market_tab import MarketTab
 from .tabs.buy_tab import BuyTab
+from .tabs.sell_tab import SellTab
 from .styles import MainTabsStyle
 
 if TYPE_CHECKING:
@@ -26,9 +27,11 @@ class BisqTab(QWidget, Logger):
         self.tabs = tabs = QTabWidget(self)
         self.market_tab = MarketTab(window, self) 
         self.buy_tab = BuyTab(window, self) 
+        self.sell_tab = SellTab(window, self) 
         tabs.setStyleSheet(MainTabsStyle)
         tabs.addTab(self.market_tab, _('Market'))
         tabs.addTab(self.buy_tab, _('Buy'))
+        tabs.addTab(self.sell_tab, _('Sell'))
         vbox.addWidget(tabs)
         vbox.setContentsMargins(2, 2, 2, 2)
 
---
+++ widgets/tabs/sell_tab.py
@@ -0,0 +1,40 @@
+
+from typing import TYPE_CHECKING, Optional
+
+from PyQt6.QtWidgets import (QVBoxLayout, QWidget, QTabWidget)
+
+from electrum.i18n import _
+from electrum.logging import Logger
+from .sell.bitcoin import SellBitcoinTab
+from .sell.bsq import SellBsqTab
+from .sell.xmr import SellXmrTab
+from .sell.other import SellOtherTab
+from ..styles import ChildTabStyle
+
+if TYPE_CHECKING:
+    from electrum.gui.qt.main_window import ElectrumWindow
+
+class SellTab(QWidget, Logger):
+
+    def __init__(self, window: 'ElectrumWindow', parent: Optional['QWidget'] = ...):
+        QWidget.__init__(self, parent)
+        Logger.__init__(self)
+
+        self.window = window
+        self.wallet = window.wallet
+        self.config = window.config
+        
+        vbox = QVBoxLayout(self)
+        self.tabs = tabs = QTabWidget(self)
+        self.sell_bitcoin_tab = SellBitcoinTab(window, self)
+        self.sell_bsq_tab = SellBsqTab(window, self)
+        self.sell_xmr_tab = SellXmrTab(window, self)
+        self.sell_other_tab = SellOtherTab(window, self)
+        tabs.addTab(self.sell_bitcoin_tab, _('Bitcoin'))
+        tabs.addTab(self.sell_bsq_tab, _('BSQ'))
+        tabs.addTab(self.sell_xmr_tab, _('XMR'))
+        tabs.addTab(self.sell_other_tab, _('Other'))
+        tabs.setStyleSheet(ChildTabStyle)
+        vbox.addWidget(tabs)
+        vbox.setContentsMargins(2, 2, 2, 2)
+
