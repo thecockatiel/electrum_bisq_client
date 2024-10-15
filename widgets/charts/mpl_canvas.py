b36b03bd07dbc980c8cff00f26b7107e3fa811e5
thecockatiel <thecockatiel@proton.me>
2024-10-15 15:33:37 +0330

    fix: plot style and figure position

--- widgets/charts/mpl_canvas.py
+++ widgets/charts/mpl_canvas.py
@@ -12,8 +12,5 @@ class MplCanvas(FigureCanvasQTAgg):
     def __init__(self, parent=None, width=5, height=4, dpi=100):
         fig = Figure(figsize=(width, height), dpi=dpi)
         self.axes = fig.add_subplot(111)
-        # make plot background transparent
-        fig.patch.set_alpha(0.3)
-        self.axes.patch.set_alpha(0)
-        fig.tight_layout()
+        fig.subplots_adjust(top=0.85, bottom=0.15)
         super(MplCanvas, self).__init__(fig)
