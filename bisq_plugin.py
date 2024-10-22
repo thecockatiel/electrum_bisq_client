250f2dfacb65dde9f5cf499f7a97f14cb119db69
thecockatiel <thecockatiel@proton.me>
2024-10-22 19:33:52 +0330

    chore: move resource reading to plugin file

--- bisq_plugin.py
+++ bisq_plugin.py
@@ -1,13 +1,32 @@
 from typing import TYPE_CHECKING
 
 from electrum.plugin import BasePlugin, hook
-from PyQt6.QtCore import QObject, QCoreApplication
 from pathlib import Path
+import os
+from functools import lru_cache
+from PyQt6.QtGui import (QIcon)
 
 if TYPE_CHECKING:
     from electrum.gui.qt import ElectrumGui
 
 from .tor import TorManager
+
+# absolute path to python package folder of electrum ("lib")
+plugin_dir = os.path.split(os.path.realpath(__file__))[0]
+
+def resource_path(*parts):
+    return os.path.join(plugin_dir, "resources", *parts)
+
+@lru_cache(maxsize=1000)
+def read_QIcon(icon_basename: str) -> QIcon:
+    return QIcon(resource_path(icon_basename))
+
+def read_resource_lines(resource_name: str) -> str:
+    try:
+        with open(resource_path(resource_name), 'r') as f:
+            return f.readlines()
+    except FileNotFoundError:
+            return None
 
 class BisqPlugin(BasePlugin):
 
--- qt.py
+++ qt.py
@@ -1,26 +1,13 @@
 from typing import TYPE_CHECKING
 
 from electrum.plugin import hook
-from .bisq_plugin import BisqPlugin
+from .bisq_plugin import BisqPlugin, read_QIcon
 from .widgets.bisq_tab import BisqTab
-import os
-from functools import lru_cache
-from PyQt6.QtGui import (QIcon)
 
 if TYPE_CHECKING:
     from electrum.gui.qt import ElectrumGui
     from electrum.gui.qt.main_window import ElectrumWindow
     from electrum.wallet import Abstract_Wallet
-    
-def resource_path(*parts):
-    return os.path.join(plugin_dir, "resources", *parts)
-
-@lru_cache(maxsize=1000)
-def read_QIcon(icon_basename: str) -> QIcon:
-    return QIcon(resource_path(icon_basename))
-
-# absolute path to python package folder of electrum ("lib")
-plugin_dir = os.path.split(os.path.realpath(__file__))[0]
 
 class Plugin(BisqPlugin):
 
